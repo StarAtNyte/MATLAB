5df3db514a5f50a5a6745b02fa68f8314b30d205
Nitiz Khanal <khanalnitij20@gmail.com>
2023-06-27 12:22:04 +0545

    GS-NR

---
+++ Power System/Load_Flow_Analysis_Gauss_Seidel.m
@@ -0,0 +1,141 @@
+%% Load Flow Analysis using Gauss-Seidel Method
+
+a = [0 0.02 0.01;
+    0.02 0 0.0125;
+    0.01 0.0125 0];
+
+b = [0 0.04 0.03;
+    0.04 0 0.025;
+    0.03 0.025 0;];
+
+LineData = complex(a,b); 
+
+Ybus = calculateYbus(LineData , 3);
+
+disp('Ybus=');
+disp(Ybus);
+
+BusTypes = [1 2 3;]; 
+Base_mva = 100;
+
+Genload = [0 0 0 0;
+    50 30 305.6 140.2;
+    0.0 0.0 138.6 45.2;];
+
+P = zeros(size(Ybus,1),1);
+Q = zeros(size(Ybus,1),1);
+
+for i = 2:3
+    P(i) = (Genload(i,1)- Genload(i,3))/Base_mva;
+    Q(i) = (Genload(i,2)- Genload(i,4))/Base_mva;
+end
+
+PQ = complex(P,Q);
+
+V = [1.05+0i;
+    1+0i;
+    1+0i;];
+
+V_old = V;
+
+
+%% (a) phasor values of the voltage at buses 2 and 3
+for i = 1:2
+    for k = 2:3
+        temp = [0;
+            0;
+            0;];
+ 
+        for n = 1:3
+            if k ~= n
+                temp(k,1) = temp(k,1) +(Ybus(k,n) * V(n,1)); 
+            end
+        
+        end
+        V(k,1) = (1/Ybus(k,k))*((conj(PQ(k,1))/V(k,1)) - temp(k,1));
+    end
+end
+disp("V=");
+disp(V);
+
+r2 = abs(V(2,1));
+theta2 = rad2deg(angle(V(2,1)));
+r3 = abs(V(3,1));
+theta3 = rad2deg(angle(V(3,1)));
+disp("(a) After 2 iterations");
+fprintf("V2 = %f<%f\n",r2,theta2);
+fprintf("V3 = %f<%f\n\n\n",r3,theta3);
+
+
+%% (b)Slack bus real and reactive power after second iteration
+SlackBusIndex = 1;
+temp = 0;
+for i = 1:3
+    temp = temp + (Ybus(SlackBusIndex,i)* V(i,1)* conj(V(SlackBusIndex,1)));
+end
+
+disp('(b) Slack bus real and reactive power after second iteration:');
+fprintf('\nP_slack = %f MW\n', (real(temp))*100);
+fprintf('Q_slack = %f MVAR\n\n\n', -(imag(temp))*100);
+
+%% (c)line flows and line losses after second iteration.
+I = zeros(3,3);
+I(1,2) = -Ybus(1,2)*(V(1,1)-V(2,1));
+I(2,1) = -I(1,2);
+I(1,3) = -Ybus(1,3)*(V(1,1)-V(3,1));
+I(3,1) = -I(1,3);
+I(3,2) = -Ybus(3,2)*(V(3,1)-V(2,1));
+I(2,3) = -I(3,2);
+
+S = zeros(3,3);
+for i= 1:3
+    for j = 1:3
+        if i~=j
+            S(i,j) = V(i,1)* conj(I(i,j));
+        end
+    end
+end
+
+disp("(c) Flow and Line Losses after 2nd iteration S = ");
+disp(S);
+
+PLoss12 = real(S(1,2)) + real(S(2,1));
+PLoss13 = real(S(1,3)) + real(S(3,1));
+PLoss23 = real(S(2,3)) + real(S(3,2));
+
+fprintf('\nPLoss12 = %f MW\n', (PLoss12*100));
+fprintf('PLoss13 = %f MW\n', (PLoss13*100));
+fprintf('PLoss23 = %f MW\n\n', (PLoss23*100));
+
+
+QLoss12 = imag(S(1,2)) + imag(S(2,1));
+QLoss13 = imag(S(1,3)) + imag(S(3,1));
+QLoss23 = imag(S(2,3)) + imag(S(3,2));
+
+fprintf('\nQLoss12 = %f MVAR\n', (QLoss12*100));
+fprintf('QLoss13 = %f MVAR\n', (QLoss13*100));
+fprintf('QLoss23 = %f MVAR\n\n\n', (QLoss23*100));
+
+%% function to calculate ybus
+function Ybus = calculateYbus(LineData, numBuses)
+
+    Ybus = zeros(numBuses, numBuses);
+    s = size(Ybus, 1);
+    for i = 1:s
+        for j = 1: size(Ybus,2)
+            data = 0;
+            if i ~=j
+                data = -(1/LineData(i,j));
+            else
+                for k = 1:3
+                    if i ~= k
+                        data = data + 1/(LineData(i,k));
+                    end
+                end
+
+            end
+                
+            Ybus(i, j) = data;
+        end
+    end
+end
---
+++ Power System/Load_Flow_Analysis_Newton_Raphson.m
@@ -0,0 +1,105 @@
+%NEWTON RAPHSON METHOD
+clc;
+gbus = [1 2.0 1.0 0.0 0.0
+2 0.0 0.0 0.5 1.0
+3 1.5 0.6 0.0 0.0];
+
+
+a = [0 0.02 0.01;
+    0.02 0 0.0125;
+    0.01 0.0125 0];
+
+b = [0 0.04 0.03;
+    0.04 0 0.025;
+    0.03 0.025 0;];
+
+LineData = complex(a,b); 
+
+ybus = calculateYbus(LineData , 3);
+
+disp('ybus=');
+disp(ybus);
+
+BusTypes = [1 2 3;]; 
+Base_mva = 100;
+
+
+v1=1.05+0i;
+v2=1+0i;
+v3=1+0i;
+
+del3=angle(v3);
+del1=angle(v1);
+del2=angle(v2);
+
+%% phasor values of the voltage at buses 2 and 3
+
+for i=1:3
+    p2 = (abs(ybus(2,1)) * abs(v1) * abs(v2)) * cos(del2-del1- angle(ybus(2,1))) + (abs(ybus(2,2)) * abs(v2) * abs(v2)) * cos(angle(ybus(2,2))) + (abs(ybus(2,3)) * abs(v2) * abs(v3)) * cos(del2-del3-angle(ybus(2,3))); 
+    q2=-(abs(v2)*abs(v1)*abs(ybus(2,1))*sin((angle(ybus(2,1)))+del1-del2))-abs(v2)*abs(v2)*abs(ybus(2,2))*sin((angle(ybus(2,2))))-(abs(v2)*abs(v3)*abs(ybus(2,3)))*sin((angle(ybus(2,3))+del3-del2));
+    p3=(abs(v3)*abs(v1)*abs(ybus(3,1))*cos((angle(ybus(3,1)))+del1-del3))+abs(v3)*abs(v3)*abs(ybus(3,3))*cos((angle(ybus(3,3))))+(abs(v2)*abs(v3)*abs(ybus(3,2)))*cos((angle(ybus(3,2))+del2-del3));
+    q3 = (abs(ybus(3,1)) * abs(v1) * abs(v3)) * sin(del3-del1- angle(ybus(3,1))) + (abs(ybus(3,2)) * abs(v2) * abs(v3)) * sin(del3-del2-angle(ybus(3,2))) - (abs(ybus(3,3)) * abs(v3)*abs(v3)) * sin(angle(ybus(3,3))); 
+    
+    J(1,1)=(abs(v2)*abs(v1)*abs(ybus(2,1))*sin((angle(ybus(2,1)))+del1-del2))+(abs(v2)*abs(v3)*abs(ybus(2,3))*sin((angle(ybus(2,3)))+del3-del2));
+    J(1,2)=(abs(v2)*abs(v3)*abs(ybus(2,3))*sin((-angle(ybus(2,3)))-del3+del2));
+    J(1,3)=(abs(v1)*abs(ybus(2,1))*cos(del2-del1-angle(ybus(2,1))))+2*(abs(v2)*abs(ybus(2,2))*cos((angle(ybus(2,2)))))+(abs(v3)*abs(ybus(2,3))*cos(del2-del3-angle(ybus(2,3))));
+    J(1,4)= (abs(v2)*abs(ybus(2,3))*cos((-angle(ybus(2,3)))-del3+del2));
+    
+    J(2,1)=-(abs(v3)*abs(v2)*abs(ybus(3,2))*sin((angle(ybus(3,2)))+del2-del3));
+    J(2,2)=(abs(v3)*abs(v1)*abs(ybus(3,1))*sin((angle(ybus(3,1)))+del1-del3))+(abs(v3)*abs(v2)*abs(ybus(3,2))*sin((angle(ybus(3,2)))+del2-del3));
+    J(2,3)=(abs(v3)*abs(ybus(3,2))*cos((angle(ybus(3,2)))+del2-del3));
+    J(2,4) =(abs(v1)*abs(ybus(3,1))*cos((-angle(ybus(3,1))-del1+del3)))+(abs(v2)*abs(ybus(3,2))*cos((-angle(ybus(3,2)))-del2+del3))+ 2*(abs(ybus(3,3))* abs(v3) * cos(angle(ybus(3,3))));
+    
+    J(3,1)=(abs(v2)*abs(v1)*abs(ybus(2,1))*cos((-angle(ybus(2,1)))-del1+del2))+(abs(v2)*abs(v3)*abs(ybus(2,3))*cos((-angle(ybus(2,3)))+del2-del3));
+    J(3,2)=-(abs(v2)*abs(v3)*abs(ybus(2,3))*cos((-angle(ybus(2,3)))+del2-del3));
+    J(3,3)=(abs(v1)*abs(ybus(2,1))*sin((-angle(ybus(2,1)))-del1+del2))+2*(abs(v2)*abs(ybus(2,2))*sin((angle(ybus(2,2)))))+ (abs(ybus(3,2))*abs(v3)*sin(-angle(ybus(2,3))));
+    J(3,4) = abs(ybus(2,3))*abs(v2)*sin(del2-del3-angle(ybus(2,3)));
+
+    J(4,1)= -(abs(ybus(3,2))*abs(v2)*abs(v3)*cos(del3-del2-angle(ybus(3,2))));
+    J(4,2)=(abs(v3)*abs(v1)*abs(ybus(3,1))*cos((-angle(ybus(3,1)))-del1+del3))+(abs(v2)*abs(v3)*abs(ybus(3,2))*cos((-angle(ybus(3,2)))-del2+del3));
+    J(4,3)= abs(ybus(3,2))*abs(v3)*sin(del3-del2-angle(ybus(3,2)));
+    J(4,4)=(abs(v1)*abs(ybus(3,1))*sin(-(angle(ybus(3,1)))))+(abs(v2)*abs(ybus(3,2))*sin(-(angle(ybus(3,2)))))+2*(abs(v3)*abs(ybus(3,3))*sin((angle(ybus(3,3)))));
+
+    
+    A=[del2;del3;abs(v2);abs(v3)];
+    M = [-2.556+0.5;
+        -1.386+0.5;
+        -1.102+1;
+        -0.45+1.5;];
+    X = [0;
+        0;
+        1;
+        1;];
+    delx = inv(J)*M;
+    delx(1,1) = rad2deg(delx(1,1));
+    delx(2,1) = rad2deg(delx(2,1));
+    X = X + delx;
+    del2 = X(1,1);
+    del3 = X(2,1);
+    v2 = X(3,1);
+    v3 = X(4,1);
+    fprintf("Iter %d\n\n",i);
+    disp("Jacobian Matrix");
+    disp(J);
+    fprintf("\n");
+    fprintf("V2 = %f\nv3 = %f\n\n\n",v2,v3);
+end
+
+
+%% Function to calculate Ybus
+function Ybus = calculateYbus(LineData, numBuses)
+    Ybus = zeros(numBuses, numBuses);
+    for i = 1:numBuses
+        for j = 1:numBuses
+            if i ~= j
+                Ybus(i,j) = -1 / LineData(i,j);
+            else
+                for k = 1:numBuses
+                    if i ~= k
+                        Ybus(i,j) = Ybus(i,j) + 1 / LineData(i,k);
+                    end
+                end
+            end
+        end
+    end
+end
